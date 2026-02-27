d2f7d4955f55245844b2341d86aee00bfa832bd9
Abderrahmane Belarbi <abdoustart007@gmail.com>
2026-02-27 12:11:04 +0100

    protecting routes

--- frontend/src/app.jsx
+++ frontend/src/app.jsx
@@ -1,5 +1,5 @@
 import AnimatedBackground from "../components/animated-background.jsx";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/home.jsx";
 import Login from "../pages/login.jsx";
 import Register from "../pages/register.jsx";
@@ -7,6 +7,25 @@ import VerifyEmail from "../pages/verify-email.jsx";
 import { useAuth } from "../store/auth-store.js";
 import { useEffect } from "react";
 import Dashboard from "../pages/dashboard.jsx";
+
+function RedirectAuthenticatedUser({ children }) {
+  const { isAuthenticated, user } = useAuth();
+  if (isAuthenticated && user.isVerified) {
+    return <Navigate to="/dashboard" replace />;
+  }
+  return children;
+}
+
+function ProtectRoute({ children }) {
+  const { isAuthenticated, user } = useAuth();
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+  if (isAuthenticated && !user.isVerified) {
+    return <Navigate to="/verify-email" replace />;
+  }
+  return children;
+}
 
 export default function App() {
   const { isCheckingAuth, checkAuth, isAuthenticated, user } = useAuth();
@@ -17,10 +36,22 @@ export default function App() {
     <AnimatedBackground>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={
+          <RedirectAuthenticatedUser>
+            <Login />
+          </RedirectAuthenticatedUser>
+        } />
+        <Route path="/register" element={
+          <RedirectAuthenticatedUser>
+            <Register />
+          </RedirectAuthenticatedUser>
+        } />
         <Route path="/verify-email" element={<VerifyEmail />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard" element={
+          <ProtectRoute>
+            <Dashboard />
+          </ProtectRoute>
+        } />
       </Routes>
     </AnimatedBackground>
   );
