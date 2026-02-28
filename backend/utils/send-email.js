caab1fbcc35cb993d723e959f7f7a1dfefe31005
Abderrahmane Belarbi <abdoustart007@gmail.com>
2026-02-28 22:12:26 +0100

    deploying

--- backend/utils/send-email.js
+++ backend/utils/send-email.js
@@ -1,11 +1,22 @@
 import { transporter } from "../config/mailer.js";
 
 export async function sendEmail({ to, subject, text, html }) {
-  return transporter.sendMail({
+  const sendMailPromise = transporter.sendMail({
     from: process.env.MAIL_FROM,
     to,
     subject,
     text,
     html,
   });
+
+  const timeoutMs = Number(process.env.SMTP_SEND_TIMEOUT_MS || 10_000);
+
+  return Promise.race([
+    sendMailPromise,
+    new Promise((_, reject) => {
+      setTimeout(() => {
+        reject(new Error(`SMTP send timeout after ${timeoutMs}ms`));
+      }, timeoutMs);
+    }),
+  ]);
 }
