07f70815c15795e6ac00f570bbd43de47704fa97
Abderrahmane Belarbi <abdoustart007@gmail.com>
2026-02-28 18:39:22 +0100

    try to diploy

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,12 @@ import { connectToDatabase } from "./database/connect-to-database.js";
 import authRoutes from "./routes/auth-routes.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-
+const __dirname = path.resolve();
 app.use(cors({origin: "http://localhost:3000", credentials: true})); // to allow cross-origin requests
 
 // Express middleware that parses incoming JSON request bodies.
@@ -18,6 +19,13 @@ app.use(express.json()); // to parse incoming requests with JSON payloads (req.b
 app.use(cookieParser()); // to parse cookies from incoming requests (req.cookies)
 
 app.use("/api/auth", authRoutes);
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  })
+}
 
 async function start() {
   await connectToDatabase();
