3bb750bf2aee6af991eb7940ab15f96829f39e96
Abderrahmane Belarbi <abdoustart007@gmail.com>
2026-02-28 20:00:15 +0100

    add secure port for production

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -3,7 +3,11 @@ import nodemailer from "nodemailer";
 export const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
-  secure: false,
+  secure: Number(process.env.SMTP_PORT) === 465,
+  connectionTimeout: 10_000,
+  greetingTimeout: 10_000,
+  socketTimeout: 10_000,
+  debug: process.env.NODE_ENV === "development",
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
