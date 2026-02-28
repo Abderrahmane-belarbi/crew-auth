f915d669f6d3a6205d3a9a8dfda688002e279c44
Abderrahmane Belarbi <abdoustart007@gmail.com>
2026-02-28 19:18:52 +0100

    deploy

--- frontend/store/auth-store.js
+++ frontend/store/auth-store.js
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 
-const API_URL = "http://localhost:5000/api/auth";
+const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api/auth" : "/api/auth";
 
 export const useAuth = create((set) => ({
   user: null,
